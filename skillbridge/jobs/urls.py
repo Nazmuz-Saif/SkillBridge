a24f78ddeef1bc63d58bef70f7e41d5bd619f8c5
Nazmuz-Saif <nazmuzsaif4080@gmail.com>
2026-05-15 03:13:55 +0600

    url

--- skillbridge/jobs/urls.py
+++ skillbridge/jobs/urls.py
@@ -5,11 +5,8 @@ urlpatterns = [
     path('apply/<int:job_id>/', views.apply_job, name='apply_job'),
     path('save/<int:job_id>/', views.savejob, name='save_job'),
     path('jobs/', views.job_list, name='job_list'),
-<<<<<<< HEAD
     path('my-applications/', views.my_applications, name='my_applications'),
     path('saved-jobs/', views.saved_jobs, name='saved_jobs'),
     path('save-job/<int:job_id>/', views.save_job, name='save_job'),
-=======
     path('post-job/', views.post_job, name='post_job'),
->>>>>>> 7ac1f6ca559ebf999b17f505d519b51aa9e0f0cc
 ]
